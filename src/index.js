2d83e473d023ebfbda23a19ac88f1028a9c34bb3
tanmay-0017 <97460992+tanmay-0017@users.noreply.github.com>
2023-04-01 18:42:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.css'
+import { faIndustry, faPlaneDeparture, faShip, faTrainSubway, faTruckField, faWarehouse } from '@fortawesome/free-solid-svg-icons'
+import video2 from '../../videos/video1.mp4'
+import { Link } from 'react-router-dom'
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  // useEffect(() => {
+  //   return setTimeout(() => {
+  //     setLetterClass('text-animate-hover')
+  //   }, 3000)
+  // }, [])ß
+
+  return (
+    <>
+      <div className="about-page">
+        <video src={video2} autoPlay loop muted></video>
+        <div className="text-zone">
+          <h1>
+            {/* <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['T', 'I', 'T', 'L', 'E', ' ', ' BATA', ' ', 'YAAR !!!!']}
+              idx={15}
+            /> */}
+            ABOUT US
+          </h1>
+          <br />  <br />
+          <p className='top_para'>
+          We are using innovative technologies to improve the efficiency and transparency of supply chain management, which can benefit businesses by helping them better serve their customers and stakeholders.
+          We have designed an approach to help small startup ventures in automating their Supply Chain in order to maximise their profit. Our model includes various Supply Chain Services like Demand Projection and Analysis, Product Lineage Tracking and a marketplace to connect ventures to the wholesale Sellers.
+          </p>
+          
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faTruckField} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faWarehouse} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faPlaneDeparture} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faShip} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faTrainSubway} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faIndustry} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
 
 
+      </div>
+      {/* <Loader type="pacman" /> */}
+    </>
+  )
+}
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+export default About
